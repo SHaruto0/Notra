6462a5d9812848723718fa3e80347923a8157341
SHaruto0 <szkharuto0407@gmail.com>
2026-02-26 23:34:05 -0500

    Added functionality to fetch note in the beginning

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,15 +2,30 @@ import SideBar from "./SideBar";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Editor from "./Editor";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const data: Note[] = await window.db.getAllNotes();
+      setNotes(data);
+    };
+
+    fetchNotes();
+  }, []);
+
   return (
     <div style={{ display: "flex" }}>
-      <SideBar /> {/* sidebar always visible */}
+      <SideBar notes={notes} setNotes={setNotes} />
       <div style={{ flex: 1, padding: "1rem" }}>
         <Routes>
           <Route path="/" element={<div>Select a note</div>} />
-          <Route path="/notes/:noteId" element={<Editor />} />
+          <Route
+            path="/notes/:noteId"
+            element={<Editor notes={notes} setNotes={setNotes} />}
+          />
         </Routes>
       </div>
     </div>
