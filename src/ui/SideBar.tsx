c152e1665e82e64c3f2772a52426f1e160bc2267
SHaruto0 <szkharuto0407@gmail.com>
2026-02-26 23:35:26 -0500

    Added functionality to adding and deleting notes

--- src/ui/SideBar.tsx
+++ src/ui/SideBar.tsx
@@ -1,15 +1,46 @@
+import type React from "react";
 import "./SideBar.css";
 import Logo from "./assets/logo.svg";
+import Trash from "./assets/trash.svg";
 import NewNote from "./assets/new-note.svg";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-function SideBar() {
+function SideBar({
+  notes,
+  setNotes,
+}: {
+  notes: Note[];
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
+}) {
+  const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
-  const fakeNotes: Note[] = [
-    { id: "1", title: "A", content: "Hello A", updatedAt: 1 },
-    { id: "2", title: "B", content: "Hello B", updatedAt: 1 },
-    { id: "3", title: "C", content: "Hello C", updatedAt: 1 },
-  ];
+
+  const createNote = async () => {
+    setLoading(true);
+    try {
+      const newNote: Note = await window.db.createNote();
+      setNotes((prev) => [newNote, ...prev]);
+      navigate(`/notes/${newNote.id}`);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteNote = async (noteID: string) => {
+    try {
+      const deletedNote: Note = await window.db.deleteNote(noteID);
+
+      setNotes((prevNotes) => {
+        return prevNotes.filter((note) => note.id !== noteID);
+      });
+      navigate("/");
+    } catch (err) {
+      console.error(`Error deleting note (${noteID})`, err);
+    }
+  };
 
   return (
     <div className="sideBar">
@@ -19,20 +50,32 @@ function SideBar() {
       </div>
       <div className="buttonContainer">
         <img src={NewNote} alt="New Note" className="newNote" />
-        <button className="newNote">New Note</button>
+        <button className="newNote" onClick={createNote} disabled={loading}>
+          {loading ? "Creating..." : "New Note"}
+        </button>
       </div>
       <div className="noteHistory">
         <div className="noteHistoryLabel">Your Notes:</div>
         <ul className="noteHistoryList">
-          {fakeNotes.map((note) => (
-            <li
-              className="noteHistoryEntry"
-              key={note.id}
-              onClick={() => {
-                navigate(`/notes/${note.id}`);
-              }}
-            >
-              {note.title}
+          {notes.map((note) => (
+            <li className="noteHistoryEntry" key={note.id}>
+              <div
+                className=""
+                onClick={() => {
+                  navigate(`/notes/${note.id}`);
+                }}
+              >
+                {note.title || "Untitled"}
+              </div>
+              <img
+                className="delete"
+                src={Trash}
+                alt="Delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteNote(note.id);
+                }}
+              />
             </li>
           ))}
         </ul>
