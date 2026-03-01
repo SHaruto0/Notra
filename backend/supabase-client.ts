dfa99ca30fa2ac2d7e7cf929212328bf50fae10e
SHaruto0 <szkharuto0407@gmail.com>
2026-02-28 22:33:35 -0500

    Only export supabase client

--- backend/supabase-client.ts
+++ backend/supabase-client.ts
@@ -3,15 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_KEY!,
 );
-
-const getNotes = async () => {
-  const { data, error } = await supabase.from("notes").select("*");
-  if (error) console.error("Error:", error);
-  else console.log("Notes:", data);
-};
-
-getNotes();
