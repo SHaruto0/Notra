7011d5d472b86b6636c6145a108eec0013defaf6
SHaruto0 <szkharuto0407@gmail.com>
2026-03-01 19:46:41 -0500

    Connected Electron to supabase via backend server (working POST and DELETE)

--- frontend/src/electron/ipcHandlers.ts
+++ frontend/src/electron/ipcHandlers.ts
@@ -1,10 +1,15 @@
-import { getAllNotes, updateNote } from "./notesSyncService.js";
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  updateNote,
+} from "./notesSyncService.js";
 import { SQLiteDatabase } from "./sqlite.js";
 import { ipcMainHandle } from "./util.js";
 
 export function setUpSQLiteHandler(db: SQLiteDatabase) {
   ipcMainHandle("getAllNotes", async () => await getAllNotes());
-  ipcMainHandle("createNote", () => db.createNote());
+  ipcMainHandle("createNote", async () => await createNote());
   ipcMainHandle("updateNote", async (params) => await updateNote(params));
-  ipcMainHandle("deleteNote", (id) => db.deleteNote(id));
+  ipcMainHandle("deleteNote", async (id) => await deleteNote(id));
 }
--- frontend/src/electron/notesSyncService.ts
+++ frontend/src/electron/notesSyncService.ts
@@ -1,30 +1,52 @@
 import { db, SQLiteDatabase } from "./sqlite.js";
 
 export const getAllNotes = async (): Promise<Note[]> => {
-  try {
-    const response = await fetch("http://localhost:3000/notes", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const response = await fetch("http://localhost:3000/notes", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (!response.ok) {
-      throw new Error(`Get request failed: ${response.status}`);
-    }
+  const data = await response.json();
 
-    const data = await response.json();
+  if (!response.ok) {
+    throw new Error(`Get request failed: ${response.status} - ${data.message}`);
+  }
 
-    const notes: Note[] = data.notes;
+  const notes: Note[] = data.notes;
 
-    for (const note of notes) {
-      db.setNote(note);
-    }
-  } catch (err) {
-    console.error(err);
+  for (const note of notes) {
+    db.setNote(note);
   }
 
   return db.getAllNotes();
+};
+
+export const createNote = async (): Promise<Note> => {
+  const note: Note = db.createNote();
+
+  const payload = {
+    id: note.id,
+    updatedAt: note.updatedAt,
+  };
+
+  const response = await fetch("http://localhost:3000/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(
+      `Create request failed: ${response.status} - ${errorData.message}`,
+    );
+  }
+
+  return note;
 };
 
 export const updateNote = async (params: {
@@ -32,24 +54,48 @@ export const updateNote = async (params: {
   title: string;
   content: string;
 }): Promise<Note> => {
-  let note: Note;
-  try {
-    note = db.updateNote(params);
+  const note = db.updateNote(params);
+  console.log("HI");
+  console.log(JSON.stringify(note));
 
-    const response = await fetch("http://localhost:3000/notes", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(note),
-    });
+  const response = await fetch("http://localhost:3000/notes", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  console.log("hello");
 
-    if (!response.ok) {
-      throw new Error(`Update request failed: ${response.status}`);
-    }
-  } catch (err) {
-    console.error(err);
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(
+      `Update request failed: ${response.status} - ${errorData.message}`,
+    );
   }
 
-  return db.getNote(params.id)!;
+  return note;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const note = db.deleteNote(id);
+
+  const response = await fetch("http://localhost:3000/notes", {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+    }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(
+      `Delete request failed: ${response.status} - ${errorData.message}`,
+    );
+  }
+
+  return note;
 };
