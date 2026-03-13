19cdb25a67d341d68fe545a65f35801e904c7a88
SHaruto0 <szkharuto0407@gmail.com>
2026-03-13 00:57:42 -0400

    Auth Context Provider

---
+++ frontend/src/ui/contexts/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import type { ReactNode } from "react";
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  function login() {
+    setIsAuthenticated(true);
+  }
+
+  function logout() {
+    setIsAuthenticated(false);
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- frontend/src/ui/main.tsx
+++ frontend/src/ui/main.tsx
@@ -4,10 +4,14 @@ import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
+import { AuthProvider } from "./contexts/AuthContext";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
+      {/* <AuthProvider> */}
       <App />
+      {/* </AuthProvider> */}
     </HashRouter>
   </StrictMode>,
 );
