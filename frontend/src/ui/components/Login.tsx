6124716e18554483d533c2e6bbdba123c3e134b7
SHaruto0 <szkharuto0407@gmail.com>
2026-03-31 22:18:28 -0400

    Add a back button

--- frontend/src/ui/components/Login.tsx
+++ frontend/src/ui/components/Login.tsx
@@ -72,6 +72,20 @@ function Login() {
 
   return (
     <div className="loginContainer">
+      <button className="goBack" onClick={() => navigate("/")}>
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M19 12H5M12 5l-7 7 7 7" />
+        </svg>
+      </button>
       <div className="loginSubContainer">
         <div className="tab-bar">
           <button
