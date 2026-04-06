60f70975f00ae8f29673a7c30829ed5de4f467e9
SHaruto0 <szkharuto0407@gmail.com>
2026-04-05 23:19:29 -0400

    Styling update

--- desktop/src/ui/App.tsx
+++ desktop/src/ui/App.tsx
@@ -10,14 +10,10 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 function App() {
   return (
     <Routes>
-      {/* <Route path="/login" element={<Login />} /> */}
-
-      {/* <Route element={<ProtectedRoute />}> */}
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<div>Select a note</div>} />
         <Route path="/notes/:noteId" element={<Editor />} />
       </Route>
-      {/* </Route> */}
 
       <Route path="/login" element={<Login />} />
     </Routes>
